66d21298ec9780be9b74dbe38d92c29dd5a2a2b9
Robert Brinkmann <>
2023-05-06 14:17:55 +1000

    Add expand/contract, update styles

--- src/components/item.tsx
+++ src/components/item.tsx
@@ -3,13 +3,21 @@ import ChildItem from "@/components/item";
 import type Child from "@/types/child";
 
 const Item = ({ item }: { item: Child }) => {
+  const [expanded, setExpanded] = useState(false);
+
   return (
     <div
       className={`item ${
         item.children && item.children.length ? "has-children" : "no-children"
-      }`}
+      } ${expanded ? "expanded" : "collapsed"}`}
     >
-      <div className="title">{item.name}</div>
+      <div className="title" onClick={() => setExpanded(!expanded)}>
+        <span className="name">{item.name}</span>
+
+        {item.children && item.children.length && (
+          <span className="symbol">{expanded ? "-" : "+"}</span>
+        )}
+      </div>
       {item.children && item.children.length && (
         <div className="children">
           {item.children.map((child: Child, index: number) => (
